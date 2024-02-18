4c39641881c40ca42c7eac95e595536c71988b3d
Shaul Fridman <shaulf@nvidia.com>
2024-02-18 21:53:45 -0200

    (+) install conan from scrach

--- examples/tools/autotools/autotoolstoolchain/string_formatter/run_example.sh
+++ examples/tools/autotools/autotoolstoolchain/string_formatter/run_example.sh
@@ -8,20 +8,26 @@ set -ex
 rm -rf conanbuild* conanrun* conanauto* deactivate* *.pc aclocal* auto* config.* Makefile.in depcomp install-sh missing Makefile configure string_formatter
 
 # Then generate conanbuild.sh
-conan install -r conancenter . --build=missing
-source conanbuild.sh
+if [[ ! -e .conan/bin/conan ]]; then
+  pip install conan --target .conan
+  .conan/bin/conan profile detect -f
+  .conan/bin/conan install --requires cmake/3.28.1 -of build
+  source ./build/conanbuild.sh
+fi
+.conan/bin/conan install -r conancenter . --build=missing -of build
+source build/conanbuild.sh
 
 # Build the example
-aclocal
-automake --add-missing
+aclocal 
+automake --add-missing 
 autoconf
 ./configure
 make
 
-source deactivate_conanbuild.sh
+source build/deactivate_conanbuild.sh
 
 # Make dynamic library available on PATH
-source conanrun.sh
+source build/conanrun.sh
 
 output=$(./string_formatter)
 
